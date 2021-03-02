749924cc6df40545dd5082ceb21e4d9a2c994377
Ishara-Mac <isharamacdonald@hotmail.com>
2021-03-02 10:36:29 +0100

    Test Driven Development: Added Round and RoundTest

--- src/main/java/nl/hu/cisq1/lingo/trainer/domain/Round.java
+++ src/main/java/nl/hu/cisq1/lingo/trainer/domain/Round.java
@@ -1,49 +1,126 @@
 package nl.hu.cisq1.lingo.trainer.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Round {
-    private static Long nextId = Long.valueOf(0);
+
+    // region parameters
+
+    private boolean roundActive = true;
+    private static Long nextId = 0L;
     private Long roundId;
-    private String wordToGuess;
-    private int currentscore;
-    private List<Feedback> allFeedback;
-    private int maxAttempts;
+
+    // region scores
+    private int maxScore = 1000;
+    private int scorePerLetter = 100;
+    private int currentScore = 0;
+    private int penalty = 10;
+    //endregion
+
+    // region attempts, word to Guess and Feedback
+    private List<String> attempts = new ArrayList<>();
+    private int maxAttempts = 1;
+
+    private String wordToGuess = "";
+
+    private List<Feedback> allFeedback = new ArrayList<>();
+    //endregion
+
+    //endregion
 
     public Round(){
-        setRoundId();
+        roundSetUp();
     }
 
-    public Round(String wordToGuess, int currentscore, int maxAttempts){
+    public Round(String wordToGuess, int maxAttempts){
         this.wordToGuess = wordToGuess;
-        this.currentscore = currentscore;
         this.maxAttempts = maxAttempts;
+        roundSetUp();
+    }
 
+    public Round(String wordToGuess, int scorePerLetter, int maxAttempts){
+        this.wordToGuess = wordToGuess;
+        this.scorePerLetter = scorePerLetter;
+        this.maxAttempts = maxAttempts;
+        roundSetUp();
+    }
+
+    private void roundSetUp(){
         setRoundId();
+        calculateMaxScore();
+        calculatePentalty();
     }
 
     //region setters and getters
+    //region setters
+    public static void setNextId(Long id){ nextId = id; }
+    public static void setNextId(int id){ nextId = (long) id; }
+
+    public void setAllFeedback(List<Feedback> allFeedback) { this.allFeedback = allFeedback;}
+    public void setCurrentScore(int currentScore) { this.currentScore = currentScore; }
+
+    public void setWordToGuess(String wordToGuess) {
+        this.wordToGuess = wordToGuess;
+        calculateMaxScore();
+    }
+
+    public void setMaxAttempts(int maxAttempts) {
+        this.maxAttempts = maxAttempts;
+        calculatePentalty();
+    }
+
+    public void setRoundId() {
+        this.roundId = nextId;
+        nextId += 1L;
+    }
+    //endregion
     //region getters
     public List<Feedback> getAllFeedback() { return allFeedback; }
     public String getWordToGuess() { return wordToGuess; }
-    public int getCurrentscore() { return currentscore; }
+    public List<String> getAttempts() { return attempts; }
+    public boolean isRoundActive() { return roundActive; }
+    public int getCurrentScore() { return currentScore; }
     public int getMaxAttempts() { return maxAttempts; }
     public static Long getNextId() { return nextId; }
+    public int getMaxScore() { return maxScore; }
     public Long getRoundId() { return roundId; }
+
+    //endregion
     //endregion
 
-    //region setters
-    public void setAllFeedback(List<Feedback> allFeedback) { this.allFeedback = allFeedback;}
-    public void setCurrentscore(int currentscore) { this.currentscore = currentscore; }
-    public void setWordToGuess(String wordToGuess) { this.wordToGuess = wordToGuess; }
-    public void setMaxAttempts(int maxAttempts) { this.maxAttempts = maxAttempts; }
-    public void setRoundId() {
-        this.roundId = nextId;
-        nextId += nextId;
+    public void addAttempt(String attempt){
+        if(allFeedback.size() < maxAttempts){
+            attempts.add(attempt);
+            processAttempts(attempt);
+        }
     }
-    //endregion
-    //endregion
 
+    public void processAttempts(String attempt){
+        if(attempt.equals(wordToGuess)){
+            roundActive = false;
+        }else{
+            calculateScore();
+        }
+        Feedback feedback = new Feedback(wordToGuess, attempt);
+        feedback.processAttempt();
+        allFeedback.add(feedback);
+    }
 
+    public void calculateScore(){
+        currentScore -= penalty;
+    }
 
+    private void calculateMaxScore(){
+        maxScore = 0;
+        for(int index = 0; index < wordToGuess.length(); index++){
+            maxScore += scorePerLetter;
+        }
+        currentScore = maxScore;
+    }
+
+    private void calculatePentalty(){
+        penalty = maxScore / maxAttempts;
+        System.out.println(penalty);
+    }
 }
--- src/test/java/nl/hu/cisq1/lingo/trainer/domain/RoundTest.java
+++ src/test/java/nl/hu/cisq1/lingo/trainer/domain/RoundTest.java
@@ -1,7 +1,126 @@
 package nl.hu.cisq1.lingo.trainer.domain;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class RoundTest {
+    Round round;
+    @BeforeEach
+    void setUp(){
+        Round.setNextId(0);
+        round = new Round();
+        round.setWordToGuess("Hey");
+    }
+
+    //region Tests for RoundId
+    @Test
+    @DisplayName("Creating a Round automatically sets the roundId")
+    void roundIdIsMade(){
+        assertEquals(0, round.getRoundId());
+    }
+
+    @Test
+    @DisplayName("A new round gets a new roundId")
+    void roundIdGetsGenerated(){
+        Round secondRound = new Round();
+        assertEquals(1, secondRound.getRoundId());
+    }
+    //endregion
+
+    //region Tests for List<Feedback> allFeedback
+    @Test
+    @DisplayName("When adding an attempt, a feedback gets added to the allFeedback list.")
+    void feedbackAddedToList(){
+        round.processAttempts("Hey");
+        assertFalse(round.getAllFeedback().isEmpty());
+    }
+
+    @Test
+    @DisplayName("When adding new attempts, feedbacks get added to the allFeedback list.")
+    void processingMultipleAttempts(){
+        round.processAttempts("Hel");
+        round.processAttempts("Hai");
+        round.processAttempts("Hey");
+        assertEquals(3, round.getAllFeedback().size());
+    }
+
+    // region Feedback and Marks
+    // Go to #FeedbackTest.java for more Feedback tests
+    @Test
+    @DisplayName("When adding a correct attempt, feedback gives back a correct mark")
+    void correctAttemptGivesBackFeedback(){
+        round.processAttempts("Hey");
+        assertEquals(List.of(Mark.CORRECT, Mark.CORRECT, Mark.CORRECT), round.getAllFeedback().get(0).getMarks());
+    }
+
+    @Test
+    @DisplayName("When adding a incorrect attempt, feedback gives back a incorrect mark")
+    void incorrectAttemptGivesBackFeedback() {
+        round.processAttempts("Hei");
+        assertEquals(List.of(Mark.CORRECT, Mark.CORRECT, Mark.ABSENT), round.getAllFeedback().get(0).getMarks());
+    }
+
+    @Test
+    @DisplayName("When adding a invalid attempt, feedback gives back invalid marks")
+    void invalidAttemptGivesBackFeedback(){
+        round.processAttempts("He");
+        assertEquals(List.of(Mark.INVALID, Mark.INVALID, Mark.INVALID), round.getAllFeedback().get(0).getMarks());
+    }
+    //endregion
+
+    // region maxAttempts
+    @Test
+    @DisplayName("There cannot be more attempts than the set maxAttempts")
+    void maxAttemptsTest(){
+        round.setMaxAttempts(3);
+
+        round.addAttempt("Hei");
+        round.addAttempt("Hei");
+        round.addAttempt("Hei");
+        round.addAttempt("Hei");
+        assertEquals(3, round.getAllFeedback().size());
+    }
+    //endregion
+    //endregion allFeedback
+
+    //region calculateScore()
+//    @Test
+//    @DisplayName("Using addScore(), score gets added to the currentScore")
+//    void addScoreTest(){
+//        round.calculateScore(1);
+//        assertEquals(1, round.getCurrentScore());
+//    }
+
+    @Test
+    @DisplayName("maxScore increases the more letter the player has to guess")
+    void maxScoreIncrease(){
+        round.setWordToGuess("hi");
+        int lowerScore = round.getMaxScore();
+        round.setWordToGuess("hello");
+        int higherScore = round.getMaxScore();
+        assertTrue(lowerScore < higherScore);
+    }
+
+    @Test
+    @DisplayName("score gives back maxScore when first attempt is correct")
+    void scoreOnFirstCorrectAttempt(){
+        round.processAttempts("Hey");
+        assertEquals(round.getMaxScore(), round.getCurrentScore());
+    }
+
+    @Test
+    @DisplayName("Score gives back lower score than maxScore when second attempt is correct")
+    void scoreOnSecondCorrectAttempt(){
+        round.setMaxAttempts(5);
+        round.processAttempts("oio");
+        round.processAttempts("Hey");
+        System.out.println(round.getCurrentScore());
+        assertTrue(round.getCurrentScore() < round.getMaxScore());
+    }
 
 }
