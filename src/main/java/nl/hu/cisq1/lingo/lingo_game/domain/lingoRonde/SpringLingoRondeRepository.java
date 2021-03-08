7759dcfa40f5a5ba5575846a12dc39027a21ad44
Ishara-Mac <isharamacdonald@hotmail.com>
2021-03-08 09:12:23 +0100

    Revert "Merge remote-tracking branch 'upstream/main' into main"
    This reverts commit 44b277d2160c5d33b5c0bbd33d5b8515eaf0b2eb, reversing
    changes made to 62f7a1d8a15bbfc0e6c8bfe6a0fe8895c63602dc.

--- src/main/java/nl/hu/cisq1/lingo/lingo_game/domain/lingoRonde/SpringLingoRondeRepository.java
+++
@@ -1,6 +0,0 @@
-package nl.hu.cisq1.lingo.lingo_game.domain.lingoRonde;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SpringLingoRondeRepository extends JpaRepository<LingoRonde,Long> {
-}
